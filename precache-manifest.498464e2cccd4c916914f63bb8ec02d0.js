bdb4cf2e12345ca72f82a2b4b688fe5c15cca90a
DominikRapacki <dr@picnature.de>
2019-08-16 12:20:05 +0200

    Updates

---
+++ precache-manifest.498464e2cccd4c916914f63bb8ec02d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7df6a2aefb007177d79bb7a8cfa7eb",
+    "url": "/simple-filter-app/index.html"
+  },
+  {
+    "revision": "cd96116977219986f024",
+    "url": "/simple-filter-app/static/css/main.23dd3fad.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/simple-filter-app/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "cd96116977219986f024",
+    "url": "/simple-filter-app/static/js/main.d7208ff5.chunk.js"
+  },
+  {
+    "revision": "cea5507f3a16d9771b75",
+    "url": "/simple-filter-app/static/js/runtime~main.488622e8.js"
+  }
+]);
